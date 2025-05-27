c008b47abda9122d1d18e36bf2fdb99d181149d6
yaroslav-volkov-dev <volkov.yaroslav94@gmail.com>
2025-05-27 18:39:01 +0300

    initial commit

---
+++ EF.Training/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using EF.Training.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.Training;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=testdb;Username=postgres;Password=1234");
+    }
+}
---
+++ EF.Training/Program.cs
@@ -0,0 +1,33 @@
+﻿using EF.Training.Services;
+
+class Program 
+{
+    static Task Main()
+    {
+        
+        UserService service = new ();
+
+        while (true)
+        {
+            Console.WriteLine("Select an action");
+
+            string? input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "GET":
+                    service.GetAllUsers();
+                    break;
+                case "POST":
+                    service.AddUser();
+                    break;
+                case "DELETE":
+                    service.DeleteUser();
+                    break;
+                default:
+                    Console.WriteLine("Something went wrong");
+                    break;
+            }
+        }
+    }
+}
